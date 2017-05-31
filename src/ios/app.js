07b0e7989614578c8a1be230a45d71a1a248125f
buiqtuan <tuanbq2909@gmail.com'git config --global user.email tuanbq2909@gmail.comgit config --global user.email "tuanbq2909@gmail.com>
2017-05-31 22:28:00 +0700

    Complete aapp

--- src/ios/app.js
+++ src/ios/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 import Firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Spinner, Button, Card } from './components/common';
 import LoginForm from './components/loginform';
 
 export default class App extends React.Component {
+    state = { 
+        loggedIn: null 
+    };
+
     componentWillMount() {
         Firebase.initializeApp({
             apiKey: 'AIzaSyB8s-GDdt18PMErXXgHKkAwjE_cOKxX_LM',
@@ -14,14 +18,46 @@ export default class App extends React.Component {
             storageBucket: 'reactauth-48f1b.appspot.com',
             messagingSenderId: '182672736736'
         });
+
+        Firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <View style={styles.buttonLogout}>      
+                        <Button onPress={() => { Firebase.auth().signOut(); }}>
+                            Log out
+                        </Button>
+                    </View>   
+                );
+            case false:
+                return (<LoginForm />);
+            default :
+                return (<Spinner size='large' />);
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    buttonLogout: {
+        height: 45,
+    }
+};
+
